e5ea0f6f3abc64bc8a0bcd8fc49eaf2fc0f3800a
mbohbless <mbohblesspearl@gmail.com>
2023-12-16 16:28:22 +0200

    running unit test

--- test/huggingface.test.ts
+++ test/huggingface.test.ts
@@ -2,16 +2,18 @@
 import HuggingFaceEmbedder from '../src/service/embedders/huggingface';
 import { Embedding } from '../src/service/types';
 
+const token = "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"
+const model_id = "sentence-transformers/all-MiniLM-L6-v2"
 describe('HuggingFaceEmbedder', ()=>{
     it('should create an instance', ()=>{
-        const embedder = new HuggingFaceEmbedder('test', 'test');
+        const embedder = new HuggingFaceEmbedder(token, model_id);
         expect(embedder).toBeInstanceOf(HuggingFaceEmbedder);
     });
 });
 
 describe('HuggingFaceEmbedder', ()=>{
     it('should test the dim', ()=>{
-        const embedder = new HuggingFaceEmbedder('test', 'test');
+        const embedder = new HuggingFaceEmbedder(token, model_id);
         expect(embedder.dim).toBe(-1);
     });
 }
@@ -19,7 +21,7 @@ describe('HuggingFaceEmbedder', ()=>{
 
 describe('HuggingFaceEmbedder', ()=>{
     it('should test the embed_batch method', ()=>{
-        const embedder = new HuggingFaceEmbedder('test', 'test');
+        const embedder = new HuggingFaceEmbedder(token, model_id);
         const embed = embedder.embed_batch(['test']);
         expect(embed).toBeInstanceOf(Promise<Embedding[]>);
     });
